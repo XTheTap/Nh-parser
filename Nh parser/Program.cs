7d0b2a82b750299a310b436a66d4dfac344918ed
XTheTap <illia9842@gmail.com>
2021-01-21 22:48:50 +0300

    Last commit

--- Nh parser/Program.cs
+++ Nh parser/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace Nh_parser
 {
@@ -10,57 +9,92 @@ namespace Nh_parser
     {
         public static void Main()
         {
-            Console.WriteLine("Input link or numbers:");
-            String numbr = Regex.Match(Console.ReadLine(),@"\d+").ToString();
-            Console.WriteLine("Go to: " + numbr);
-
-            try
+            while (true)
             {
+                Console.Clear();
+                Console.WriteLine("Input link or numbers:");
+                //Taking the numbers of manga
+                String numbr = Regex.Match(Console.ReadLine(), @"\d+").ToString();
+                Console.WriteLine("Going to: " + numbr);
+
                 using (WebClient wc = new WebClient())
                 {
-                    wc.Headers.Add("2XTheTap", "Mozilla/4.0 (compatible; MSIE 6.0; Windows 10; .NET CLR 1.0.3705;)");;
+                    //Set name of parser
+                    wc.Headers.Add("NhParser", "Mozilla/4.0 (compatible; MSIE 6.0; Windows 10; .NET CLR 1.0.3705;)");
                     
-                    string line = wc.DownloadString("https://nhentai.net/g/" + numbr);
+                    string line = "";
+
+                    try
+                    {
+                        //Writing all our page to 
+                        line = wc.DownloadString("https://nhentai.net/g/" + numbr);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("That thing dosen't wanna to go by the link, " +
+                                          "maybe deal with your internet or your numbers, lol. Exception: \n" +
+                                          e.Message + "\nPress any key to continue");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    //Making one name form author, name of manga and last
                     string name = Regex.Match(line, @"<span class=.before.>(.+?)</span>").Groups[1].ToString() +
                                   Regex.Match(line, @"<span class=.pretty.>(.+?)</span>").Groups[1] +
                                   Regex.Match(line, @"<span class=.after.>(.+?)</span>").Groups[1];
 
+                    //Replacing special symbols, becouse 
+                    name = Regex.Replace(name, "[\\|?/*:\"<>]", " "); 
+
+                    //Parsing page value
                     uint pageValue = Convert.ToUInt32(Regex.Match(line, @"<span class=.name.>(\d+)</span>")
                         .Groups[1].ToString());
 
-                    var tempRg = Regex.Match(line, @"<meta itemprop=.image. content=.+\/galleries\/(\d+)/\w+(.\w+)");
-                    
-                    uint downloadId = Convert.ToUInt32(tempRg
-                        .Groups[1].ToString());
-                    
-                    string picForm = tempRg
-                        .Groups[2].ToString();
-                    
                     Console.WriteLine("Name: " + name + " with pages value: " + pageValue);
+
                     Console.WriteLine("Are you sure that you want download it? [Y/n]: ");
-                    bool ans = Console.ReadLine().ToLower() == "y";
-                        
-                    if (!ans) return;
+                    if (Console.ReadLine().ToLower() != "y") continue;
 
-                    try { Directory.CreateDirectory(Environment.GetEnvironmentVariable("USERPROFILE") 
-                                                    + "/Downloads/" + name); }
-                    catch { Console.WriteLine("Cant create directory"); throw; }
-                    
-                    for (uint i = 1; i < pageValue + 1; i++)
+                    //Creating directory in download folder 
+                    try
                     {
-                        wc.DownloadFile(
-                            new Uri("https://i.nhentai.net/galleries/" + downloadId + "/" + i + picForm),
-                            Environment.GetEnvironmentVariable("USERPROFILE")
-                            + "/Downloads/" + name + "/" + i + picForm);
-                        Console.WriteLine("Picture: " + i + " Done");
+                        Directory.CreateDirectory(Environment.GetEnvironmentVariable("USERPROFILE") + "/Manga/" + name);
                     }
-                    
-                    Console.WriteLine("Done");
-                }
-            } catch (Exception e) { Console.WriteLine("That shit dosent download, maybe deal with your internet, exception: \n" + e); }
+                    catch
+                    {
+                        Console.WriteLine("Cant create directory \nPress any key to continue");
+                        Console.ReadKey();
+                        continue;
+                    }
 
-            Console.WriteLine("Press any key to clese this shit");
-            Console.ReadKey();
+                    //Downloading all pages in loop
+                    try
+                    {
+                        foreach (Match i in Regex.Matches(line,
+                        //parsing info about id manga in site database, page number and image format
+                            @"img\ssrc=.https:\/\/t.nhentai.net\/galleries\/(\d+)\/(\d+)t(.\w+)."))
+                        {//downloading parsed file
+                            wc.DownloadFile(
+                                new Uri("https://i.nhentai.net/galleries/" + i.Groups[1] + "/" + i.Groups[2] +
+                                        i.Groups[3]),
+                                Environment.GetEnvironmentVariable("USERPROFILE") + "/Manga/"
+                                + name + "/" + i.Groups[2] + i.Groups[3]);
+                            Console.WriteLine("Page: " + i.Groups[2] + " ...Done");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("That thing dosen't download, maybe deal with your internet, exception: \n" +
+                                          e.Message + "\nPress any key to continue");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    Console.Clear();
+                    Console.WriteLine("All Done, Are you want to continue [Y/n]");
+                    if (Console.ReadLine().ToLower() != "y") break;
+                }
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+#Nh parser
+Well, i think you alredy know what is that for nhentai.net
+
+##Download
+You can download last release in button "release" on this page
+
+##Usage
+Just write the "numbers" of manga that you need in start menu, or input link
+Parser will ask you "Are you sure?", you just need to type 'y' or 'Y'
+
+##Why?
+So, you also can press button "download" in the site, but its just gives you link to the torrent.
+Torrent are without seeds and if it is, torrent can loads more than hour
+And soem providers, like my, can block the torrents
+So, im do solution
